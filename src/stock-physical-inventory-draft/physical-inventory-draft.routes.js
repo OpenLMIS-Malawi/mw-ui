4b9008015f388b1b44cc05e2d1dc950926a499eb
Nikola Laskowska <nicola.laskowska@gmail.com>
2021-09-13 08:36:52 +0200

    MALAWISUP-3076: Sort products alphabetically in Physical Inventory screen

--- src/stock-physical-inventory-draft/physical-inventory-draft.routes.js
+++ src/stock-physical-inventory-draft/physical-inventory-draft.routes.js
@@ -75,7 +75,8 @@
 
                     return paginationService.registerList(validator, $stateParams, function() {
                         var searchResult = physicalInventoryService.search($stateParams.keyword, draft.lineItems);
-                        var lineItems = $filter('orderBy')(searchResult, 'orderable.productCode');
+                        // MALAWISUP-3076: Products ordered alphabetically
+                        var lineItems = $filter('orderBy')(searchResult, 'orderable.fullProductName');
 
                         var groups = _.chain(lineItems).filter(function(item) {
                             var hasQuantity = !(_.isNull(item.quantity) || _.isUndefined(item.quantity));
